0fe3d0673dabf9d031139b268ed4c8ca74fdf23a
shimmh <31092342+shimmh@users.noreply.github.com>
2021-12-26 22:46:14 +0900

    로그인 페이지 개발

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -8,9 +8,13 @@ function registerUser(userData) {
   return instance.post('signup', userData);
 }
 
+function loginUser(userData) {
+  return instance.post('login', userData);
+}
+
 // function registerUser(userData) {
 //   const url = 'http://localhost:3000/signup';
 //   return axios.post(url, userData);
 // }
 
-export { registerUser };
+export { registerUser, loginUser };
